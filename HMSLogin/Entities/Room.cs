9d3c3e7095ba723f785c390f1cf13f74f30a543f
Bryan McMahon <bryanvmcmahon@gmail.com>
2020-02-11 15:32:55 +0000

    Set up LINQ and objects for beds

---
+++ HMSLogin/Entities/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMSLogin.Entities
+{
+    class Room
+    {
+        int roomId { get; set; }
+        int wardId { get; set; }
+        roomType roomType { get; set; }
+    }
+    enum roomType { PUBLIC, SEMIPRIVATE, PRIVATE }
+}
