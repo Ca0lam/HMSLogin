797f8c40b245f25fda375cb0e654f17902a0ad20
Bryan McMahon <bryanvmcmahon@gmail.com>
2020-02-18 11:02:33 +0000

    Added skeleton of beds module

---
+++ HMSLogin/HospitalLayout.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace HMSLogin
+{
+    partial class HospitalLayout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Btn_New_Bed = new System.Windows.Forms.Button();
+            this.Btn_New_Room = new System.Windows.Forms.Button();
+            this.Btn_New_Ward = new System.Windows.Forms.Button();
+            this.Btn_New_Department = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Btn_View_Bed = new System.Windows.Forms.Button();
+            this.Btn_View_Room = new System.Windows.Forms.Button();
+            this.Btn_View_Beds = new System.Windows.Forms.Button();
+            this.Btn_View_Department = new System.Windows.Forms.Button();
+            this.Btn_View_Hospital = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Btn_New_Bed);
+            this.groupBox1.Controls.Add(this.Btn_New_Room);
+            this.groupBox1.Controls.Add(this.Btn_New_Ward);
+            this.groupBox1.Controls.Add(this.Btn_New_Department);
+            this.groupBox1.Location = new System.Drawing.Point(81, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(176, 87);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "New:";
+            // 
+            // Btn_New_Bed
+            // 
+            this.Btn_New_Bed.Location = new System.Drawing.Point(89, 50);
+            this.Btn_New_Bed.Name = "Btn_New_Bed";
+            this.Btn_New_Bed.Size = new System.Drawing.Size(75, 23);
+            this.Btn_New_Bed.TabIndex = 3;
+            this.Btn_New_Bed.Text = "Bed";
+            this.Btn_New_Bed.UseVisualStyleBackColor = true;
+            // 
+            // Btn_New_Room
+            // 
+            this.Btn_New_Room.Location = new System.Drawing.Point(7, 50);
+            this.Btn_New_Room.Name = "Btn_New_Room";
+            this.Btn_New_Room.Size = new System.Drawing.Size(75, 23);
+            this.Btn_New_Room.TabIndex = 2;
+            this.Btn_New_Room.Text = "Room";
+            this.Btn_New_Room.UseVisualStyleBackColor = true;
+            // 
+            // Btn_New_Ward
+            // 
+            this.Btn_New_Ward.Location = new System.Drawing.Point(89, 20);
+            this.Btn_New_Ward.Name = "Btn_New_Ward";
+            this.Btn_New_Ward.Size = new System.Drawing.Size(75, 23);
+            this.Btn_New_Ward.TabIndex = 1;
+            this.Btn_New_Ward.Text = "Ward";
+            this.Btn_New_Ward.UseVisualStyleBackColor = true;
+            // 
+            // Btn_New_Department
+            // 
+            this.Btn_New_Department.Location = new System.Drawing.Point(7, 20);
+            this.Btn_New_Department.Name = "Btn_New_Department";
+            this.Btn_New_Department.Size = new System.Drawing.Size(75, 23);
+            this.Btn_New_Department.TabIndex = 0;
+            this.Btn_New_Department.Text = "Department";
+            this.Btn_New_Department.UseVisualStyleBackColor = true;
+            this.Btn_New_Department.Click += new System.EventHandler(this.Btn_New_Department_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Btn_View_Bed);
+            this.groupBox2.Controls.Add(this.Btn_View_Room);
+            this.groupBox2.Controls.Add(this.Btn_View_Beds);
+            this.groupBox2.Controls.Add(this.Btn_View_Department);
+            this.groupBox2.Controls.Add(this.Btn_View_Hospital);
+            this.groupBox2.Location = new System.Drawing.Point(81, 144);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(176, 110);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "View:";
+            // 
+            // Btn_View_Bed
+            // 
+            this.Btn_View_Bed.Location = new System.Drawing.Point(89, 79);
+            this.Btn_View_Bed.Name = "Btn_View_Bed";
+            this.Btn_View_Bed.Size = new System.Drawing.Size(75, 23);
+            this.Btn_View_Bed.TabIndex = 4;
+            this.Btn_View_Bed.Text = "Bed";
+            this.Btn_View_Bed.UseVisualStyleBackColor = true;
+            // 
+            // Btn_View_Room
+            // 
+            this.Btn_View_Room.Location = new System.Drawing.Point(7, 80);
+            this.Btn_View_Room.Name = "Btn_View_Room";
+            this.Btn_View_Room.Size = new System.Drawing.Size(75, 23);
+            this.Btn_View_Room.TabIndex = 3;
+            this.Btn_View_Room.Text = "Room";
+            this.Btn_View_Room.UseVisualStyleBackColor = true;
+            // 
+            // Btn_View_Beds
+            // 
+            this.Btn_View_Beds.Location = new System.Drawing.Point(89, 50);
+            this.Btn_View_Beds.Name = "Btn_View_Beds";
+            this.Btn_View_Beds.Size = new System.Drawing.Size(75, 23);
+            this.Btn_View_Beds.TabIndex = 2;
+            this.Btn_View_Beds.Text = "Ward";
+            this.Btn_View_Beds.UseVisualStyleBackColor = true;
+            // 
+            // Btn_View_Department
+            // 
+            this.Btn_View_Department.Location = new System.Drawing.Point(7, 50);
+            this.Btn_View_Department.Name = "Btn_View_Department";
+            this.Btn_View_Department.Size = new System.Drawing.Size(75, 23);
+            this.Btn_View_Department.TabIndex = 1;
+            this.Btn_View_Department.Text = "Department";
+            this.Btn_View_Department.UseVisualStyleBackColor = true;
+            // 
+            // Btn_View_Hospital
+            // 
+            this.Btn_View_Hospital.Location = new System.Drawing.Point(7, 20);
+            this.Btn_View_Hospital.Name = "Btn_View_Hospital";
+            this.Btn_View_Hospital.Size = new System.Drawing.Size(157, 23);
+            this.Btn_View_Hospital.TabIndex = 0;
+            this.Btn_View_Hospital.Text = "Hospital";
+            this.Btn_View_Hospital.UseVisualStyleBackColor = true;
+            // 
+            // HospitalLayout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 318);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "HospitalLayout";
+            this.Text = "HospitalLayout";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Btn_New_Bed;
+        private System.Windows.Forms.Button Btn_New_Room;
+        private System.Windows.Forms.Button Btn_New_Ward;
+        private System.Windows.Forms.Button Btn_New_Department;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Btn_View_Bed;
+        private System.Windows.Forms.Button Btn_View_Room;
+        private System.Windows.Forms.Button Btn_View_Beds;
+        private System.Windows.Forms.Button Btn_View_Department;
+        private System.Windows.Forms.Button Btn_View_Hospital;
+    }
+}
