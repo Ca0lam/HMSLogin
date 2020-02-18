6ffef9eca1c7f80b1caec4491db696fbbbfd0d30
Bryan McMahon <bryanvmcmahon@gmail.com>
2020-02-18 11:37:11 +0000

    Fix merge issues

---
+++ HMSLogin/BillingTestClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMSLogin
+{
+    class BillingTestClass
+    {
+        public string Name;
+        public string Id;
+        public DateTime Date;
+        public string PatientId;
+        public double RoomCharge;
+        public double DoctorsFee;
+        public string Note;
+        public double MiscFee;
+        public double TotalFee;
+
+        public override string ToString()
+        {
+            String billString = "Name: " + Name +
+                "\nId: " + Id +
+                "\nDate: " + Date.ToString("dd/MM/yy") +
+                "\nPatientId: " + PatientId +
+                "\nRoomCharge: " + RoomCharge +
+                "\nDoctorsFee: " + DoctorsFee +
+                "\nNote: " + Note +
+                "\nMiscFee: " + MiscFee +
+                "\nTotalFee: " + TotalFee;
+
+            return billString;
+        }
+    }
+}
