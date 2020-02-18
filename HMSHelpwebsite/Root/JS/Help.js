a4a83089cde5d95e9097da9fa5c3ede7411331e2
Ca0lam <54100203+Ca0lam@users.noreply.github.com>
2020-02-18 10:59:36 +0000

    updated website

--- HMSHelpwebsite/Root/JS/Help.js
+++ HMSHelpwebsite/Root/JS/Help.js
@@ -6,7 +6,39 @@ $(function () {
 });
 
 function addevents() {
+     timer();
+     getLocation();
+}
 
+function getLocation() {
+     if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+     }else {
+          console.log("Geolocation is not supproted by htis browser");
+     }
+}
 
+function showPosition(position) {
+     console.log("Latitude: " + position.coords.latitude +
+     "<br>Longitude: " + position.coords.longitude);
+}
 
+var idleTime = 0;
+$(document).ready(function () {
+     var idleInterval = setInterval(timer, 60000);
+
+     $(this).mousemove(function (e) {
+          idleTime = 0;
+     });
+     $(this).keypress(function (e) {
+          idleTime = 0;
+     });
+});
+
+function timer() {
+     idleTime = idleTime + 1;
+     if (idleTime > 5) {
+          window.location.reload();
+          localStorage.clear();
+     }
 }
