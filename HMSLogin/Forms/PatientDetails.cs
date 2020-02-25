2125940b2730c843efe6809754b304c2c4c970f0
aaronkirk <aaron.vj.kirk@gmail.com>
2020-02-25 15:29:18 +0000

    Added search features and patient notes to patient details module.

--- HMSLogin/Forms/PatientDetails.cs
+++ HMSLogin/Forms/PatientDetails.cs
@@ -23,27 +23,34 @@ namespace HMSLogin
 		{
 			InitializeComponent();
 			hospitalMS = new HospitalMSDataContext();
-			formLoad();
+			FormLoad();
 		}
 
-		private void createPatientObject()
+		private void CreatePatientObject()
 		{
 
 		}
 
 
-		private void formLoad()
+		private void FormLoad()
 		{
+			ClearComboBox();
 			int patientID = hospitalMS.tblPatientDetails.FirstOrDefault().PatientId;
 			//int patientID = 1006;
+			PopulateForm(patientID);
+			PopulateComboBox(patientID);
+		}
 
+
+		private void PopulateForm(int patientID)
+		{
 			TxtPatientID.Text = patientID.ToString();
 			TxtForename.Text = hospitalMS.tblPatientDetails.Single(x => x.PatientId == patientID).PatientForename;
 			TxtSurname.Text = hospitalMS.tblPatientDetails.Single(x => x.PatientId == patientID).PatientSurename;
 			TxtDOBDay.Text = hospitalMS.tblPatientDetails.Single(x => x.PatientId == patientID).PatientDOB.Day.ToString("D2");
 			TxtDOBMonth.Text = hospitalMS.tblPatientDetails.Single(x => x.PatientId == patientID).PatientDOB.Month.ToString("D2");
 			TxtDOBYear.Text = hospitalMS.tblPatientDetails.Single(x => x.PatientId == patientID).PatientDOB.Year.ToString();
-			
+
 			// Only One of these will end up checked
 			RdoMale.Checked = hospitalMS.tblPatientDetails.Single(x => x.PatientId == patientID).PatientGender;
 			RdoFemale.Checked = !hospitalMS.tblPatientDetails.Single(x => x.PatientId == patientID).PatientGender;
@@ -74,18 +81,16 @@ namespace HMSLogin
 			{
 				Console.WriteLine(row);
 			}
-
 		}
 
-		private void getPatientByID()
+		private void GetPatientByID()
 		{
 
 		}
 
 
-		private void savePatientDetails()
+		private void SavePatientDetails()
 		{
-
 
 			tblPatientDetail patient = new tblPatientDetail();
 			patient.PatientForename = TxtForename.Text;
@@ -119,16 +124,198 @@ namespace HMSLogin
 			patient.PatientNOK = JSONresult;
 
 			hospitalMS.tblPatientDetails.InsertOnSubmit(patient);
+			////hospitalMS.tblPatientDetails.UpdateOnSubmit(patient);
 			hospitalMS.SubmitChanges();
+			////hospitalMS.tblPatientDetails.SaveChanges();
 			Console.WriteLine(JSONresult);
 
 		}
 
 		private void BtnSave_Click(object sender, EventArgs e)
 		{
-			savePatientDetails();
+			SavePatientDetails();
+		}
+
+		private void BtnSearch_Click(object sender, EventArgs e)
+		{
+			Search();
+		}
+
+		private void TxtSearch_TextChanged(object sender, EventArgs e)
+		{
+			Search();
 		}
 
 
+
+		private void LstSearch_SelectedValueChanged(object sender, EventArgs e)
+		{
+			RefreshFormFromSearch();
+		}
+
+		private void Search()
+		{
+			LstSearch.Items.Clear();
+			var searchBox = TxtSearch.Text;
+			if (!string.IsNullOrWhiteSpace(searchBox))
+			{
+				var searchResult = hospitalMS.tblPatientDetails.Where(x => x.PatientSurename.Contains(searchBox) || x.PatientId.ToString().Contains(searchBox)).ToList();
+				////var patDet = from tblPatientDetail Select * ;
+				Console.WriteLine(searchResult);
+				////Console.WriteLine(patDet);
+				foreach (var item in searchResult)
+				{
+					Console.WriteLine(item.PatientForename);
+					Console.WriteLine(item.PatientSurename);
+					Console.WriteLine(item.PatientDOB);
+					LstSearch.Items.Add(item.PatientId + " " + item.PatientForename + " " + item.PatientSurename + "\n\r");
+				}
+			}
+
+		}
+
+		private void RefreshFormFromSearch()
+		{
+			var selectedValue = LstSearch.SelectedItem.ToString();
+			Console.WriteLine(selectedValue);
+			string[] splitValue = selectedValue.Split(' ');
+			Console.WriteLine(splitValue[0].ToString());
+			////new[] mystring = selectedValue.Split(' ');
+			////String[] Split(selectedValue, " ")
+			int selectedPatientID = Int32.Parse(splitValue[0]);
+			PopulateForm(selectedPatientID);
+			PopulateComboBox(selectedPatientID);
+		}
+
+
+		private void UpdatePatientDetails()
+		{
+			int patientID = Int32.Parse(TxtPatientID.Text);
+			using (HospitalMSDataContext hospitalMS = new HospitalMSDataContext())
+			{
+				// "id" is the id in your table (parameter passed)
+				//Patient patient = hospitalMS.tblPatientDetails.Find(patientID).First();   // Find may be only for Entity Framework. If you can't use it, use the line below
+				
+				
+				
+				var patient = hospitalMS.tblPatientDetails.Where(x => x.PatientId == patientID).First();
+
+				patient.PatientForename = TxtForename.Text;
+				patient.PatientSurename = TxtSurname.Text;
+				var patientDOB = Convert.ToDateTime(TxtDOBYear.Text + "-" + TxtDOBMonth.Text + "-" + TxtDOBDay.Text);
+				patient.PatientDOB = patientDOB;
+				
+				if (RdoMale.Checked)
+				{
+					patient.PatientGender = true;
+				}
+				else if (RdoFemale.Checked)
+				{
+					patient.PatientGender = false;
+				}
+				else
+				{
+					Console.WriteLine("Gender Error");
+				}
+
+				patient.PatientAddress = TxtAddress.Text;
+				patient.PatientPhoneNum = TxtPhoneNum.Text;
+
+				NOK nok = new NOK();
+				nok.NOKName = TxtNOKName.Text;
+				nok.NOKPhoneNum = TxtNOKPhoneNum.Text;
+				string JSONresult = JsonConvert.SerializeObject(nok);
+				patient.PatientNOK = JSONresult;
+
+				hospitalMS.SubmitChanges();
+			};
+		}
+
+
+		////private void CreatePatientObject2(int patientID, out Patient patient)
+		////{
+			////Patient patient = new Patient();
+			////patient.PatientForename = TxtForename.Text;
+			////patient.PatientSurename = TxtSurname.Text;
+
+			////// Format the three text fields for DOB as a string and convert to a DateTime Type
+			////var patientDOB = Convert.ToDateTime(TxtDOBYear.Text + "-" + TxtDOBMonth.Text + "-" + TxtDOBDay.Text);
+			////patient.PatientDOB = patientDOB;
+			////Console.WriteLine(patientDOB);
+
+			////if (RdoMale.Checked)
+			////{
+			////	patient.PatientGender = true;
+			////}
+			////else if (RdoFemale.Checked)
+			////{
+			////	patient.PatientGender = false;
+			////}
+			////else
+			////{
+			////	Console.WriteLine("Gender Error");
+			////}
+
+			////patient.PatientAddress = TxtAddress.Text;
+			////patient.PatientPhoneNum = TxtPhoneNum.Text;
+
+			////NOK nok = new NOK();
+			////nok.NOKName = TxtNOKName.Text;
+			////nok.NOKPhoneNum = TxtNOKPhoneNum.Text;
+			////string JSONresult = JsonConvert.SerializeObject(nok);
+			////patient.PatientNOK = JSONresult;
+		////}
+
+		private void BtnUpdate_Click(object sender, EventArgs e)
+		{
+			UpdatePatientDetails();
+		}
+
+
+		private void PopulateComboBox(int patientId)
+		{
+
+			var listofnotes = hospitalMS.tblPatientNotes.Where(x => x.PatientId.Equals(patientId)).ToList();
+			foreach (var item in listofnotes)
+			{
+				CbxPatientNotes.Items.Add(item.NoteDate.ToString());
+			}
+		
+
+			////CbxPatientNotes.Items.Add("One");
+			////CbxPatientNotes.Items.Add("Two");
+			////CbxPatientNotes.Items.Add("Three");
+			////CbxPatientNotes.Items.Add("Four");
+			////CbxPatientNotes.Items.Add("Five");
+			////CbxPatientNotes.Items.Add("Six");
+			////CbxPatientNotes.Items.Add("Seven");
+
+		}
+
+		private void ClearComboBox()
+		{
+			CbxPatientNotes.Items.Clear();
+		}
+
+
+		private void OpenNote()
+		{
+
+		}
+
+		private void NewNote()
+		{
+
+		}
+
+		private void btnNewPatientNote_Click(object sender, EventArgs e)
+		{
+			new PatientNotes(5).ShowDialog();
+		}
+
+		private void BtnViewNote_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
---
+++ HMSLogin/Forms/PatientNotes.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace HMSLogin
+{
+	partial class PatientNotes
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.TxtNoteBody = new System.Windows.Forms.TextBox();
+			this.BtnSave = new System.Windows.Forms.Button();
+			this.LblDate = new System.Windows.Forms.Label();
+			this.LblPatientId = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// TxtNoteBody
+			// 
+			this.TxtNoteBody.Location = new System.Drawing.Point(53, 89);
+			this.TxtNoteBody.Multiline = true;
+			this.TxtNoteBody.Name = "TxtNoteBody";
+			this.TxtNoteBody.Size = new System.Drawing.Size(274, 284);
+			this.TxtNoteBody.TabIndex = 0;
+			// 
+			// BtnSave
+			// 
+			this.BtnSave.Location = new System.Drawing.Point(252, 399);
+			this.BtnSave.Name = "BtnSave";
+			this.BtnSave.Size = new System.Drawing.Size(75, 23);
+			this.BtnSave.TabIndex = 1;
+			this.BtnSave.Text = "Save";
+			this.BtnSave.UseVisualStyleBackColor = true;
+			// 
+			// LblDate
+			// 
+			this.LblDate.AutoSize = true;
+			this.LblDate.Location = new System.Drawing.Point(53, 47);
+			this.LblDate.Name = "LblDate";
+			this.LblDate.Size = new System.Drawing.Size(44, 13);
+			this.LblDate.TabIndex = 2;
+			this.LblDate.Text = "LblDate";
+			// 
+			// LblPatientId
+			// 
+			this.LblPatientId.AutoSize = true;
+			this.LblPatientId.Location = new System.Drawing.Point(237, 47);
+			this.LblPatientId.Name = "LblPatientId";
+			this.LblPatientId.Size = new System.Drawing.Size(63, 13);
+			this.LblPatientId.TabIndex = 3;
+			this.LblPatientId.Text = "LblPatientId";
+			// 
+			// PatientNotes
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(377, 458);
+			this.Controls.Add(this.LblPatientId);
+			this.Controls.Add(this.LblDate);
+			this.Controls.Add(this.BtnSave);
+			this.Controls.Add(this.TxtNoteBody);
+			this.Name = "PatientNotes";
+			this.Text = "PatientNotes";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox TxtNoteBody;
+		private System.Windows.Forms.Button BtnSave;
+		private System.Windows.Forms.Label LblDate;
+		private System.Windows.Forms.Label LblPatientId;
+	}
+}
